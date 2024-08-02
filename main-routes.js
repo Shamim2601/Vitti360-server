543366ccaedb8c21d9bcfc77fb57b6bec7595d2e
Shamim Al Mamun <shamim2601@gmail.com>
2024-08-02 16:06:59 +0600

    tutor route

--- main-routes.js
+++ main-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Question = require('./models/Question'); // Make sure the path is correct
+const Tutor = require('./models/Tutor'); // Make sure the path is correct
 
 // Define other routes
 router.get('/', (req, res) => {
@@ -23,10 +23,7 @@ router.post('/data', (req, res) => {
 // GET /jiggasa route
 router.get('/jiggasa', async (req, res) => {
   try {
-    // Define your tags
     const tags = ['bangla', 'english', 'science', 'history', 'religion', 'math', 'IQ', 'other'];
-
-    // Select a random tag from the tags array
     const randomCategory = tags[Math.floor(Math.random() * tags.length)];
 
     const category = req.query.category || randomCategory;
@@ -34,28 +31,52 @@ router.get('/jiggasa', async (req, res) => {
 
     let questions;
     if (questionId) {
-      // Find the question by the custom 'id' field
       const question = await Question.findOne({ id: questionId });
-
-      // Check if the question was found
       if (!question) {
         return res.status(404).json({ message: 'Question not found' });
       }
-
       questions = [question];
     } else {
-      // Find questions by category
       questions = await Question.find({ category }).sort({ id: 1 });
     }
 
-    res.json({
-      questions,
-      category,
-    });
-
+    res.json({ questions, category });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+// GET /tutors route
+router.get('/tutors', async (req, res) => {
+  try {
+    const req_tag = req.query.tag || 'all';
+    const searchQuery = req.query.query;
+    const tags = ['buet', 'du', 'medical', 'cadet'];
+    let data;
+
+    if (searchQuery) {
+      const searchRegex = new RegExp(searchQuery, 'i');
+      data = await Tutor.find({
+        $or: [
+          { name: searchRegex },
+          { pref: searchRegex },
+          { institution: searchRegex },
+          { dept: searchRegex },
+          { college: searchRegex },
+          { expertise: searchRegex }
+        ]
+      }).sort({ rating: -1, id: 1 });
+    } else if (req_tag !== 'all') {
+      data = await Tutor.find({ tag: req_tag }).sort({ rating: -1, id: 1 });
+    } else {
+      data = await Tutor.find({ tag: { $in: tags } }).sort({ tag: 1, rating: -1, id: 1 });
+    }
+
+    res.json({ data, currentTag: req_tag, searchQuery });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Server error' });
   }
 });
 
